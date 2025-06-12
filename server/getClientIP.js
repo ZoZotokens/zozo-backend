7f0954e29901d23b26128fe87bb388e369c241d7
ZoZoTokens <info@zozoofficial.org>
2025-06-13 02:36:52 +0330

    Update getClientIP.js

--- server/getClientIP.js
+++ server/getClientIP.js
@@ -1,7 +1,6 @@
 function getClientIP(req) {
   const xForwardedFor = req.headers['x-forwarded-for'];
   if (xForwardedFor) {
-    // اگر چند IP هست، اولین IP واقعی رو برمی‌گردونه
     return xForwardedFor.split(',')[0].trim();
   }
   return req.socket.remoteAddress || null;
